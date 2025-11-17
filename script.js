f9d7842e4d0d0fe66c22794451cde6722ad5e589
Hhimanhsu Kumawat <himanshhukumawat2031@gmail.com>
2025-11-17 14:09:15 +0530

    Update script.js
    Comment out the load event listener and downshift function.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-window.addEventListener('load', () => {
-  document.querySelectorAll('.down-but').forEach(function (el) {
-    el.classList.remove('rotate');
-  });
-});
+// window.addEventListener('load', () => {
+//   document.querySelectorAll('.down-but').forEach(function (el) {
+//     el.classList.remove('rotate');
+//   });
+// });
 
 
-let downshift=(val)=>{
-  document.querySelector(`.quin${val}`).classList.toggle('ques-out')
-  document.querySelector(`.r${val}`).classList.toggle('rotate')
-  document.querySelector(`.r${val}`).classList.toggle('rorev')
-}
+// let downshift=(val)=>{
+//   document.querySelector(`.quin${val}`).classList.toggle('ques-out')
+//   document.querySelector(`.r${val}`).classList.toggle('rotate')
+//   document.querySelector(`.r${val}`).classList.toggle('rorev')
+// }
 
 
